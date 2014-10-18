59d9e31514bb4cf31a4e7fa16502610a9dedf35e
ziyang <ziyang0621@gmail.com>
2014-10-18 15:03:56 -0700

    added blur effect and gif

--- Twitter/ProfileViewController.swift
+++ Twitter/ProfileViewController.swift
@@ -13,15 +13,16 @@ protocol ProfileViewControllerDelegate {
     optional func toggleLeftPanel()
 }
 
-class ProfileViewController: UIViewController {
+let kHeaderHeight:CGFloat = 200
+let kNavigatonBarPlusStatusBarHeight:CGFloat = 64
+
+class ProfileViewController: UIViewController, UIGestureRecognizerDelegate, UITableViewDelegate, UITableViewDataSource{
     
     var userName: String = ""
     
     var userInfo: User?
     
     var delegate: ProfileViewControllerDelegate?
-    
-    var refreshControl = UIRefreshControl()
     
     var fromMenu = true
 
@@ -41,9 +42,15 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var followerCountLabel: UILabel!
     
+    @IBOutlet weak var headerView: UIView!
+    
+    var effectView: UIVisualEffectView!
+    
+    var viewDidAppear = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if fromMenu {
             self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Menu", style: UIBarButtonItemStyle.Plain, target: self, action: "onMenu")
         } else {
@@ -51,12 +58,23 @@ class ProfileViewController: UIViewController {
         }
         
         self.navigationItem.title = "Profile"
-                
-        refreshControl.addTarget(self, action: "refreshProfile", forControlEvents: UIControlEvents.ValueChanged)
-        tableView.addSubview(refreshControl)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        var effect = UIBlurEffect(style: .Light)
+        effectView = UIVisualEffectView(effect: effect)
+        effectView.alpha = 0
         
         refreshProfile()
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        effectView.frame = bannerImageView.frame
+        bannerImageView.addSubview(effectView)
+        viewDidAppear = true
+    }
+    
  
     func refreshProfile() {
         var param = ["screen_name" : userName]
@@ -81,10 +99,39 @@ class ProfileViewController: UIViewController {
             if let followerCount = user?.followerCount {
                 self.followerCountLabel.text = "\(followerCount)"
             }
-            
-            self.refreshControl.endRefreshing()
         })
 
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let yPos: CGFloat = -scrollView.contentOffset.y
+        
+        if yPos > 0 && viewDidAppear {
+            var imgRect = effectView.frame
+            imgRect.origin.y = scrollView.contentOffset.y + kNavigatonBarPlusStatusBarHeight
+            imgRect.size.height = kHeaderHeight+yPos
+            effectView.frame = imgRect
+            effectView.alpha = 1
+        }
+    }
+    
+
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        UIView.beginAnimations("fade in", context: nil)
+        UIView.setAnimationDuration(0.5)
+        effectView.alpha = 0
+        UIView.commitAnimations()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("profileCell") as UITableViewCell
     }
 
     override func didReceiveMemoryWarning() {
@@ -99,7 +146,7 @@ class ProfileViewController: UIViewController {
     }
     
     func onClose() {
-       dismissViewControllerAnimated(true, completion: nil)
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
 
